06ec3c5eff37f5791058001c9c6bb590cf38629d
Gerry Huang <gerryhzgtest2021@yahoo.com>
2021-08-28 20:35:33 -0700

    eks using terraform

---
+++ modules/eks/securitygroup.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "demo-cluster" {
+  name        = "${var.cluster-name}-cluster-sg"
+  description = "cluster communication with worker nodes"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.cluster-name}-cluster-sg"
+  }
+}
+resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
+  description = "Allow pods to communicate with the cluster API Server"
+  from_port = 443
+  protocol = "tcp"
+  security_group_id = aws_security_group.demo-cluster.id
+  source_security_group_id = aws_security_group.demo-node.id
+  to_port = 443
+  type = "ingress"
+}
+resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
+  cidr_blocks = [local.workstation-external-cidr]
+  description = "Allow workstation to communicate with the cluster API Server"
+  from_port = 443
+  protocol = "tcp"
+  security_group_id = aws_security_group.demo-cluster.id
+  to_port = 443
+  type = "ingress"
+}
